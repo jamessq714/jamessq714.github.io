b44c7d956a0ae7f3bec6421a084c027d8212e5b9
james squires <jamessquires714@gmail.com>
2013-08-14 18:32:21 -0700

    final project

---
+++ per's_bikes/js/translations.js
@@ -0,0 +1,577 @@
+var translations = {
+  
+//general stuff
+quick_contact: {
+	eng: 'Give us a call! (415)955-7699',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+home_tab: {
+	eng: 'Home',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_tab: {
+	eng: 'About',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_tab: {
+	eng: 'Tours',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+faq_tab: {
+	eng: 'FAQ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+book_now_tab: {
+	eng: 'Book Now',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+contact_us_tab: {
+	eng: 'Contact Us',
+	swe: 'asd',
+	ity: 'asdfd'
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//home page
+welcome: {
+	eng: "Welcome to Per's Bike Tours!",
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+home_p_1: {
+	eng: 'We’re a small, unique bike tour company in San Francisco! We want you to enjoy your day in the Bay Area countryside to the fullest. To ensure this, we provide personal service, and we keep our groups to a maximum of 12 people* per tour. No matter what shape you are in, we know that you will enjoy your day with us. All of our tours are available in English, Swedish, German, and Italian. Give us a call today to book your tour! ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+home_p_2: {
+	eng: '*Larger private group tours given if prearranged',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//about page
+
+about_h1_1: {
+	eng: 'Per (pronounced “pear”)',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_h2_1: {
+	eng: 'Owner & guide',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_p_1: {
+	eng: 'Per is the founder and inspiration for this company. Per has lived in San Francisco and guided tours since 2010. He has been guiding bike and walking tours in Europe since 2003. Per is able to lead tours in Italian, Swedish, English, and German.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_p_2: {
+	eng: 'I want to show the beauty of San Francisco that I have come to love to every guest who comes on the tour”.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_p_3: {
+	eng: 'What can you expect from us? –Well, when I say us, it is actually me, Per, whom you will be dealing with from A to Z.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_p_4: {
+	eng: 'I have worked for many other tour companies prior to starting Per’s Bike Tours, and one thing I learned from those experiences is that people like being in a smaller group, compared to being one of a big heard. I would often find myself alone, leading groups of 20+ people with little or no chance of interacting with the majority of the people. This was not something I would have signed up for, had I been on the other side of the handlebar.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+about_p_5: {
+	eng: 'Therefore, Per’s Bike Tours is unique in that you will be in a small group, you will be given personal service while dealing with the me, the owner who is 100% invested in his guests.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tours page
+tours_h1_1: {
+	eng: 'Tours',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+tours_h3_1: {
+	eng: 'San Francisco to Sausalito Classic Ride',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_1: {
+	eng: '9 miles/15 km, level 2, 3 hours. $39 + bike rent.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_1: {
+	eng: 'This classic bike ride shows our guests the best the Bay Area has to offer: Panoramic views of the Northern Californian landscape, the Golden Gate Bridge, and an optional ferry ride back to the City. ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_2: {
+	eng: 'This tour is ideal for those who would like to see the beauty of the Bay Area while enjoying a fun bike ride with superb views of the Bay. And you’ll be biking on the world famous Golden Gate Bridge!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_3: {
+	eng: 'Our group sizes are at most 10 people, so we are able to ride at a relaxed pace with many stops to enjoy the uniqueness of the San Francisco Bay Area. The ride includes 3 moderate climbs. ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_4: {
+	eng: 'If you want to experience all this, but you don\'t feel as though you can physically manage the ride, we suggest that you rent an electrical bicycle that will make the uphills a breeze!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_2: {
+	eng: 'Option:',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_5: {
+	eng: 'For those who want to bike more, this ride can be extended to Tiburon. This option adds another 10 miles/16 km to the tour for only $20. There is a ferry from Tiburon that will bring us back to SF Pier 41. It’s a great option if you have more energy and you want to see more of the country side. ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+tours_h2_3: {
+	eng: 'Detailed Description of San Francisco to Sausalito Tour ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_6: {
+	eng: '• We leave at 12 noon from GGB Rental, See map. Please be here around 11:40 AM for bike fitting.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_7: {
+	eng: '• Briefing on the handling of the bike, overview of bike safety and the route for the day.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_8: {
+	eng: '• We then begin our ride, making frequent stops as needed for hydration and photos. Remember, it’s a ride, not a race!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_9: {
+	eng: '• Around 3 PM we arrive in Sausalito.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_10: {
+	eng: '• The rest of the day is spent as you wish. For a $4 fee, you can return the bike near the ferry terminal in SF instead of the Haight St. shop.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+
+
+
+tours_h3_2: {
+	eng: 'Lazy day in the Park',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_4: {
+	eng: '8 miles/13 km, level 1, 2 hours. $29 + bike rent.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_11: {
+	eng: 'This is the bicycle tour for those who want to enjoy biking in San Francisco while exerting a minimal amount of effort. The ride begins and ends in the Haight neighborhood, located across from Golden Gate Park. ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_12: {
+	eng: 'We go for a loop in Golden Gate Park, all the way out to Ocean Beach, where the brave ones can go for a dip in the waves! On this tour we’ll ride by the Conservatory of Flowers, The Bison paddock, the DeYoung museum, the Academy of Sciences, Stow Lake, the windmills, and more. The tour offers some spectacular views of the Pacific Ocean and interesting and fun facts about this great park.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+ 
+tours_h2_5: {
+	eng: 'Detailed Description of Lazy Day in the Park Tour',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_13: {
+	eng: '• We leave at 12 noon from GGB Rental, See map. Please be here around 11:40 AM for bike fitting.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_14: {
+	eng: ' • Briefing on the handling of the bike, overview of bike safety and the route for the day.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_15: {
+	eng: '• We then begin our ride, making frequent stops as needed for hydration and photos. Remember, it’s a ride, not a race!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_16: {
+	eng: '• We usually come back to the shop at around 2 PM. However, feel free to use the bike the whole day if you so wish. We’ll be more than happy to point out fun places to go in the neighborhood!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+
+tours_h3_3: {
+	eng: 'Marin Headlands Tour',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_6: {
+	eng: '22 miles/37 km, level 3, 5 hours. $49 + bike rent.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_17: {
+	eng: 'Here’s a tour that will make you break a sweat! Still, we will take plenty of breaks to take in the scenery. We’ll cross Golden Gate Bridge and then head up the very steep road to Hawk Hill. Have you ever wondered where those scenic bridge & city shots of San Francisco are taken? Wonder no more, you are here! A great descent takes us down to Point Bonita lighthouse where we sit down to enjoy a packed lunch. ',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_18: {
+	eng: 'After lunch we head back across the GGB and back to the shop.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_7: {
+	eng: 'Detailed Description of Marin Headlands Tour',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_19: {
+	eng: '• We leave at 11 AM from GGB Rental, See map. Please be here around 10:40 AM for bike fitting.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_20: {
+	eng: '• Briefing on the handling of the bike, overview of bike safety and the route for the day.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_21: {
+	eng: '• We then begin our ride, making frequent stops as needed for hydration and photos. Remember, it’s a ride, not a race!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_22: {
+	eng: '• We usually come back to the shop at around 4 PM. However, feel free to use the bike the whole day if you so wish. We’ll be more than happy to point out fun places to go in the neighborhood!',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_p_23: {
+	eng: '• The bike has to be returned to the shop by 7 PM.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+tours_h3_4: {
+	eng: 'Private Tour & Custom Tour',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+tours_h2_8: {
+	eng: 'We are glad to offer private tours. Any of the tours described above can be a private tour on request. We can also create a custom tour for you based on your wishes. Contact us for prices and availability.',
+	swe: 'asdfadsfasd',
+	ity: 'asdafsdfasd' 
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//faq section
+
+
+faq_h1_1:{ 
+	eng: 'FAQ',
+	swe: 'asdf',
+	ity: 'asdf'
+}, 
+
+faq_h3_1:{
+	eng: 'How big are your groups?',
+	swe: 'asdf',
+	ity: 'asdfd'
+},
+
+faq_p_1: {
+	eng: 'To ensure your comfort and safety, we guarantee no more than 12 people per tour.  If there are more people in you group, we are happy to provide you with a private, prearranged tour.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_p_2: {
+	eng: 'Our minimum group size is 2 people.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h3_2:{
+	eng: 'What is the cost per tour?',
+	swe: 'asdf',
+	ity: 'asdfd'
+},
+
+faq_p_3: {
+	eng: 'The tour costs $29-$49, depending on which tour you choose. Add to this the rent cost for your bike. This will range from $14 for the 2-hour tour, and upwards to $60 for a tandem bike for a full day.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_p_4: {
+	eng: 'Some people will want a high performance bicycle, while some are happy with a basic, 21-speed hybrid bike. There are also electrical bikes which are pricier to rent, but are a great tool if you are not used to riding up hills. Also, if you come with your own bicycle, you save on not having to rent one. Typically, for the Lazy Day in the Park Tour, the cost for someone would be: $29 for the tour + $14 for a comfort hybrid bike = $43. Renting the bicycle for the whole day would be $30.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h3_3:{
+	eng: 'When do you operate?',
+	swe: 'asdf',
+	ity: 'asdfd'
+},
+
+faq_p_5: {
+	eng: 'We operate year round – every day of the week with these exceptions: Jan. 1st, Feb. 5th, Mar. 15th, July 8th, Dec. 25th, and Dec. 31st. In case of other days of closure, please check our website.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h3_4:{
+	eng: 'Can kids come on the tour?',
+	swe: 'asdf',
+	ity: 'asdfd'
+},
+
+faq_p_6: {
+	eng: 'Absolutely! We collaborate with a bike rental shop that has kids’ bikes, tagalongs, and children’s seats for rent. Please ask us if you have any concerns.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h3_5:{
+	eng: 'What does the level of difficulty mean?',
+	swe: 'asdf',
+	ity: 'asdfd'
+},
+
+faq_p_7: {
+	eng: 'Great question! It’s hard to make a scale that describes accurately all of our guest’s feelings of what is hard or easy. So we made a brave attempt at least!',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h2_1:{
+	eng: 'Level 1 ride: ',
+	swe: 'asdf',
+	ity: 'asdf'
+},
+
+faq_p_8: {
+	eng: 'Minimal physical exertion is required. Very slight grade changes, and therefore mostly flat riding.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h2_2:{
+	eng: 'Level 2 ride:',
+	swe: 'asdf',
+	ity: 'asdf'
+},
+
+faq_p_9: {
+	eng: 'More physically challenging, yet still ok for people who have limited riding experiences. Some uphill riding. ',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_h2_3:{
+	eng: 'Level 3 ride: ',
+	swe: 'asdf',
+	ity: 'asdf'
+},
+
+faq_p_10: {
+	eng: 'A good workout for most. You will be riding a good amount of uphills. Not suitable for beginners.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+faq_p_11: {
+	eng: 'This scale is meant only as a guideline, and we believe that riders of all levels will thoroughly enjoy any of the three rides. An electrical bike is a great option for those who feel that they might struggle with the uphills.',
+	swe: 'asdf',
+	ity: 'asdd'
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//book now section
+
+
+book_now_h1_1:{
+	eng: 'Book Now',
+	swe: '1232123',
+	ity: 'asdfasdf'
+},
+
+
+book_now_h2_1:{
+	eng: 'We are still setting up an online booking tool.  For now, please just let us know what you are interested and we will contact you right away about our availability',
+	swe: '1232123',
+	ity: 'asdfasdf'
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
